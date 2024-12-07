494e566a1a22b3d782c5b28ebeb6f8736be54de4
Guo Dong <gd@Foos-MacBook-Pro.local>
2024-12-07 09:39:40 +0800

    Done with registration UI

--- client/src/app/component/home/home.component.ts
+++ client/src/app/component/home/home.component.ts
@@ -3,6 +3,8 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
+import {MatGridListModule} from '@angular/material/grid-list';
+
 
 
 @Component({
@@ -13,37 +15,77 @@ import {MatDividerModule} from '@angular/material/divider';
     MatCardModule,
     MatIconModule,
     MatDividerModule,
+    MatGridListModule,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  steps = [
+  games = [
     {
-      icon: 'play_arrow',
-      title: 'Buyer or Seller Initiate Trade',
-      // description: 'The buyer or seller starts the trade.'
+      title: 'Maplestory',
+      image: 'assets/image/home_component/games/maplestory.png'
     },
     {
-      icon: 'check_circle',
-      title: 'Other Party Accepts Trade',
-      // description: 'The counterparty accepts the terms of the trade.'
+      title: 'Clash Of Clans',
+      image: 'assets/image/home_component/games/clash_of_clans.jpeg'
     },
     {
-      icon: 'credit_card',
-      title: 'Buyer Pays Escrow',
-      // description: 'The buyer pays the agreed amount into escrow.'
+      title: 'CS GO',
+      image: 'assets/image/home_component/games/cs_go.png'
     },
     {
-      icon: 'assignment_turned_in',
-      title: 'Seller Releases Game Item',
-      // description: 'The seller releases the game item to the buyer.'
+      title: 'Dota',
+      image: 'assets/image/home_component/games/dota.png'
     },
     {
-      icon: 'attach_money',
-      title: 'Escrow Releases Payment to Seller',
-      // description: 'The escrow releases the payment to the seller.'
+      title: 'Genshin Impact',
+      image: 'assets/image/home_component/games/genshin_impact.png'
+    },
+    {
+      title: 'Honkai Star Rail',
+      image: 'assets/image/home_component/games/honkai_star_rail.jpg'
+    },
+    {
+      title: 'Pokemon GO',
+      image: 'assets/image/home_component/games/pokemon_go.png'
+    },
+    {
+      title: 'Team Fortress 2',
+      image: 'assets/image/home_component/games/team_fortress_2.png'
+    },
+    {
+      title: 'Wuthering Waves',
+      image: 'assets/image/home_component/games/wuthering_waves.png'
     },
 
   ]
+  // steps = [
+  //   {
+  //     icon: 'play_arrow',
+  //     title: 'Buyer or Seller Initiate Trade',
+  //     // description: 'The buyer or seller starts the trade.'
+  //   },
+  //   {
+  //     icon: 'check_circle',
+  //     title: 'Other Party Accepts Trade',
+  //     // description: 'The counterparty accepts the terms of the trade.'
+  //   },
+  //   {
+  //     icon: 'credit_card',
+  //     title: 'Buyer Pays Escrow',
+  //     // description: 'The buyer pays the agreed amount into escrow.'
+  //   },
+  //   {
+  //     icon: 'assignment_turned_in',
+  //     title: 'Seller Releases Game Item',
+  //     // description: 'The seller releases the game item to the buyer.'
+  //   },
+  //   {
+  //     icon: 'attach_money',
+  //     title: 'Escrow Releases Payment to Seller',
+  //     // description: 'The escrow releases the payment to the seller.'
+  //   },
+
+  // ]
 }
