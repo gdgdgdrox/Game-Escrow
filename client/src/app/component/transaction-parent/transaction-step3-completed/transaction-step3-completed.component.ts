e3d66c980f29d4d85b44a0536bb5789a134d4206
Guo Dong <gd@Foos-MacBook-Pro.local>
2024-12-10 21:13:48 +0800

    UI improvement

--- client/src/app/component/transaction-parent/transaction-step3-completed/transaction-step3-completed.component.ts
+++ client/src/app/component/transaction-parent/transaction-step3-completed/transaction-step3-completed.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { TransactionResponseDTO } from '../../../dto/transaction-response.dto';
 import { AuthService } from '../../../service/auth.service';
+import { MatIconModule } from '@angular/material/icon';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-transaction-step3-completed',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, DatePipe],
   templateUrl: './transaction-step3-completed.component.html',
   styleUrl: './transaction-step3-completed.component.css'
 })
