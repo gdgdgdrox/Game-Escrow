ecbb8bd9aaf16c7bd8452813b5d02cc47a00edee
Guo Dong <gd@Foos-MacBook-Pro.local>
2024-12-25 20:47:14 +0800

    Complete code for dialog box

--- client/src/app/component/transaction/step4/transaction-step4-buyer/transaction-step4-buyer.component.ts
+++ client/src/app/component/transaction/step4/transaction-step4-buyer/transaction-step4-buyer.component.ts
@@ -7,6 +7,8 @@ import { Router } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmItemReceivedDialogComponent } from '../confirm-item-received-dialog/confirm-item-received-dialog.component';
 
 @Component({
   selector: 'app-transaction-step4-buyer',
@@ -23,26 +25,36 @@ export class TransactionStep4BuyerComponent {
   constructor(
     private transactionStep4Service: TransactionStep4Service,
     private transactionStateService: TransactionStateService,
-    private router: Router
+    private router: Router,
+    private dialog: MatDialog
   ) {}
 
   confirmItemReceived(): void {
-    this.transactionStep4Service
-      .buyerConfirmItemReceived(this.transaction.transactionID)
-      .subscribe({
-        next: (transaction: TransactionResponseDTO) => {
-          this.transactionStateService.transaction = transaction;
-          console.log('shared service state updated. navigating to parent');
-          this.router.navigate([
-            '/transaction',
-            `step${transaction.currentStep}`,
-            transaction.transactionID,
-          ]);
-        },
-        error: (error: HttpErrorResponse) => {
-          console.log(error);
-          this.itemReceivedStatusMessage = 'Something went wrong. Please try again later.';
-        },
-      });
+    const dialogRef = this.dialog.open(ConfirmItemReceivedDialogComponent);
+    dialogRef.afterClosed().subscribe(proceed => {
+      if (proceed){
+        this.transactionStep4Service
+          .buyerConfirmItemReceived(this.transaction.transactionID)
+          .subscribe({
+            next: (transaction: TransactionResponseDTO) => {
+              this.transactionStateService.transaction = transaction;
+              console.log('shared service state updated. navigating to parent');
+              this.router.navigate([
+                '/transaction',
+                `step${transaction.currentStep}`,
+                transaction.transactionID,
+              ]);
+            },
+            error: (error: HttpErrorResponse) => {
+              console.log(error);
+              this.itemReceivedStatusMessage = 'Something went wrong. Please try again later.';
+            },
+          });
+      }
+      else{
+        // no op
+      }
+    })
+
   }
 }
